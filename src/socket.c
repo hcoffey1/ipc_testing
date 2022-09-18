07f75b4ccf52833b512a0ccc5cdf7c23da914a8f
hcoffey1 <hcoffey1@vols.utk.edu>
2022-09-18 18:19:10 -0500

    Add comments

--- src/socket.c
+++ src/socket.c
@@ -9,35 +9,39 @@
 int main(int argc, char **argv)
 {
 
+    // Parse command line arguments----------
     if (argc != 3)
     {
         fprintf(stderr, "Usage: %s s/c port\n", argv[0]);
         return 1;
     }
 
+    // Determine if we are host/client and the port number
     int isHost = argv[1][0] == 's';
-
     size_t PORT = atoi(argv[2]);
 
+    // Get socket FD
     int socketfd = socket(AF_LOCAL, SOCK_STREAM, 0);
-
     if (socketfd < 0)
     {
         fprintf(stderr, "Failed to open socket.\n");
         return 1;
     }
 
+    // Fill out server address socket struct
     struct sockaddr_in servAddr = get_server_address(PORT);
 
+    // Host code---------------------
     if (isHost)
     {
+        // Bind socket to host address
         if (bind(socketfd, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0)
         {
-            // fprintf(stderr, "Bind failure.\n");
             perror("bind");
             return 1;
         }
 
+        // Start listening for connections
         if (listen(socketfd, 1) < 0)
         {
             perror("listen");
@@ -46,6 +50,7 @@ int main(int argc, char **argv)
 
         fprintf(stderr, "Server: Listening on port %lu for connections.\n", PORT);
 
+        // Host loop, accept new connections and recieve messages from clients
         while (1)
         {
             struct sockaddr_in clientAddr;
@@ -72,14 +77,17 @@ int main(int argc, char **argv)
             close(clientfd);
         }
     }
+    // Client code----------------------
     else
     {
+        // Connect to server address
         if (connect(socketfd, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0)
         {
             perror("connect");
             return 1;
         }
 
+        // Send data
         puts("Connected to server, sending message.");
 
         write(socketfd, "Hello world!", strlen("Hello world!"));
