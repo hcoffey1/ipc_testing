bc89341e6acf5157c9e80ae064363fde0b109b7d
hcoffey1 <hcoffey1@vols.utk.edu>
2022-09-25 15:47:29 -0500

    Wrap socket in API

--- src/socket.c
+++ src/socket.c
@@ -22,6 +22,151 @@ void print_log_header()
     printf("===============================================\n");
 }
 
+int open_socket()
+{
+    int socketfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketfd < 0)
+    {
+        fprintf(stderr, "Failed to open socket.\n");
+        return 1;
+    }
+
+    return socketfd;
+}
+
+int host_server(size_t PORT)
+{
+    // Get socket FD
+    int socketfd = open_socket();
+
+    // Fill out server address socket struct
+    struct sockaddr_in servAddr = get_server_address(PORT);
+
+    // Bind socket to host address
+    if (bind(socketfd, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0)
+    // if (bind(socketfd, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0)
+    {
+        perror("bind");
+        exit(1);
+    }
+
+    // Start listening for connections
+    if (listen(socketfd, 1) < 0)
+    {
+        perror("listen");
+        exit(1);
+    }
+
+    fprintf(stderr, "Server: Listening on port %lu for connections.\n", PORT);
+
+    // Host loop, accept new connections and run test
+    struct sockaddr_in clientAddr;
+    socklen_t len = sizeof(clientAddr);
+
+    int clientfd = accept(socketfd, (struct sockaddr *)&clientAddr, &len);
+    if (clientfd < 0)
+    {
+        perror("accept");
+        exit(1);
+    }
+
+    return clientfd;
+}
+
+int client_connect(size_t PORT)
+{
+    // Get socket FD
+    int socketfd = open_socket();
+
+    // Fill out server address socket struct
+    struct sockaddr_in servAddr = get_server_address(PORT);
+
+    // Connect to server address
+    if (connect(socketfd, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0)
+    {
+        perror("connect");
+        exit(1);
+    }
+
+    return socketfd;
+}
+
+void send_messages(void *data, int fd, size_t messageSize, size_t numMessages, size_t remainderSize)
+{
+    size_t offset = 0;
+
+    // Send messages over socket
+    for (int m = 0; m < numMessages; m++)
+    {
+        write(fd, data + offset, MESSAGE_SIZE);
+        offset += MESSAGE_SIZE;
+    }
+
+    // Write remaining data if any
+    if (remainderSize)
+    {
+        write(fd, data + offset, remainderSize);
+    }
+}
+
+void send_metadata(int numMessages, int remainderSize, int fd)
+{
+    // Tell reciever how many messages to expect
+    write(fd, &numMessages, sizeof(numMessages));
+
+    // Tell reciever if we need to send a message with the remaining data
+    write(fd, &remainderSize, sizeof(remainderSize));
+}
+
+void send_kill(fd)
+{
+    int tmp = -1;
+    write(fd, &tmp, sizeof(tmp));
+    close(fd);
+}
+
+int read_metadata(int socketfd, int *numMessages, int *remainder)
+{
+    // Read in number of messages to expect
+    while (1)
+    {
+        if (read(socketfd, numMessages, sizeof(*numMessages)) != 0)
+        {
+            break;
+        }
+    }
+
+    // Termination signal
+    if (*numMessages < 0)
+    {
+        return -1;
+    }
+
+    // Read number of remaining bytes
+    read(socketfd, remainder, sizeof(*remainder));
+
+    return 0;
+}
+
+void read_messages(void *destbuffer, int fd, int messageSize, int numMessages, int remainder)
+{
+    // Read in message
+    int i = 0;
+    while (i < numMessages)
+    {
+        if (read(fd, destbuffer, messageSize) != 0)
+        {
+            i++;
+        }
+    }
+
+    // Read remainder if needed
+    if (remainder)
+    {
+        read(fd, destbuffer, remainder);
+    }
+}
+
 // https://opensource.com/article/19/4/interprocess-communication-linux-networking
 
 int main(int argc, char **argv)
@@ -50,139 +195,60 @@ int main(int argc, char **argv)
     size_t PORT = atoi(argv[2]);
     MESSAGE_SIZE = 1ul << atoi(argv[3]);
 
-    // Get socket FD
-    int socketfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (socketfd < 0)
-    {
-        fprintf(stderr, "Failed to open socket.\n");
-        return 1;
-    }
-
-    // Fill out server address socket struct
-    struct sockaddr_in servAddr = get_server_address(PORT);
-
     // Host code---------------------
     if (isHost)
     {
+
         NUM_ITERATIONS = 1ul << atoi(argv[4]);
-
-        // Bind socket to host address
-        if (bind(socketfd, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0)
-        // if (bind(socketfd, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0)
-        {
-            perror("bind");
-            return 1;
-        }
-
-        // Start listening for connections
-        if (listen(socketfd, 1) < 0)
-        {
-            perror("listen");
-            return 1;
-        }
-
-        fprintf(stderr, "Server: Listening on port %lu for connections.\n", PORT);
 
         void *data = map_file(INPUT_FILE, &FILE_SIZE);
         int numMessages = FILE_SIZE / MESSAGE_SIZE;
         int remainderSize = FILE_SIZE % MESSAGE_SIZE;
 
+        // Print log information at top of run
         print_log_header();
-        // Host loop, accept new connections and run test
-        struct sockaddr_in clientAddr;
-        socklen_t len = sizeof(clientAddr);
 
-        int clientfd = accept(socketfd, (struct sockaddr *)&clientAddr, &len);
-        if (clientfd < 0)
-        {
-            perror("accept");
-            return 1;
-        }
+        // Open server and get fd pointing to client
+        int clientfd = host_server(PORT);
 
+        // Work loop
         for (int i = 0; i < NUM_ITERATIONS; i++)
         {
-            size_t offset = 0;
+            // Let the client know how many messages to expect
+            send_metadata(numMessages, remainderSize, clientfd);
 
-            // Tell reciever how many messages to expect
-            write(clientfd, &numMessages, sizeof(numMessages));
-
-            // Tell reciever if we need to send a message with the remaining data
-            write(clientfd, &remainderSize, sizeof(remainderSize));
-
-            // Send messages over socket
-            for (int m = 0; m < numMessages; m++)
-            {
-                write(clientfd, data + offset, MESSAGE_SIZE);
-                offset += MESSAGE_SIZE;
-            }
-
-            // Write remaining data if any
-            if (remainderSize)
-            {
-                write(clientfd, data + offset, remainderSize);
-            }
+            // Send the messages
+            send_messages(data, clientfd, MESSAGE_SIZE, numMessages, remainderSize);
         }
 
-        int tmp = -1;
-        write(clientfd, &tmp, sizeof(tmp));
-
-        close(clientfd);
+        // Close client
+        send_kill(clientfd);
     }
     // Client code----------------------
     else
     {
+        // Connect to server
+        int hostfd = client_connect(PORT);
+
+        // Example work loop
         char *buf = malloc(MESSAGE_SIZE);
-
-        // Connect to server address
-        if (connect(socketfd, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0)
-        {
-            perror("connect");
-            return 1;
-        }
-
         int numMessages;
         int remainder;
         int iteration = 0;
         while (1)
         {
-            // Read in number of messages to expect
-            int x;
-            while (1)
-            {
-                if (read(socketfd, &numMessages, sizeof(numMessages)) != 0)
-                {
-                    break;
-                }
-            }
-
-            // Termination signal
-            if (numMessages < 0)
+            // Read in how many messages to expect
+            if (read_metadata(hostfd, &numMessages, &remainder) == -1)
             {
                 break;
             }
-
-            // Read number of remaining bytes
-            read(socketfd, &remainder, sizeof(remainder));
 
 #ifdef SHOW_ITERATION
             printf("Iteration : %d\n", iteration);
 #endif
 
-            // Read in message
-            int i = 0;
-            while (i < numMessages)
-            {
-                if (read(socketfd, buf, MESSAGE_SIZE) != 0)
-                {
-                    i++;
-                }
-            }
-
-            // Read remainder if needed
-            if (remainder)
-            {
-                read(socketfd, buf, remainder);
-            }
+            // Read messages from host
+            read_messages(buf, hostfd, MESSAGE_SIZE, numMessages, remainder);
 
             iteration++;
         }
